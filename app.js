c2a9b193973370ae7e4c16d3dc079cb49753c353
Bálint Csertán <csertanb@gmail.com>
2023-10-22 22:09:16 +0200

    Selecting elements

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-$('#app').click(() => {
-    $('#app').html('Balint Csertan');
-}).html('Click me')
+// $('#app').click(() => {
+//     $('#app').html('Balint Csertan');
+// }).html('Click me')
+
+const elements = $('div');
+console.log(elements);
+console.log(elements[1]);
+
+elements.html('hello');
+
+const elementWithClass = $('.div2');
+console.log(elementWithClass);
+
+const elementWithId = $('#div3');
+console.log(elementWithId);
+
+console.dir(document);
